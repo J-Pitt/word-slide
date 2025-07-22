6d38b918af8db3b6b1deadd5071aee78e03236af
Jason Pittman <jpittman@Jasons-MacBook-Pro.local>
2025-07-21 20:31:59 -0400

    changing words

--- src/main.js
+++ src/main.js
@@ -733,8 +733,8 @@ function shuffleArray(arr) {
 
 // Word sets for each level - now with longer words for 7x7 board
 const PROD_WORD_SETS = [
-    ["kyle", "matt", "mike"],    // Level 1
-    ["jason", "brandon"],        // Level 2
+    ["high", "bird", "cat"],    // Level 1
+    ["dog", "hat", "mara"],        // Level 2
     ["red", "blue", "pink"],     // Level 3
     ["sun", "moon", "star"],     // Level 4
     ["book", "read", "write"],   // Level 5
